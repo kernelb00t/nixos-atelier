f4555a11801f38d0691ab8519e112acc6a1fa63d
Leo thivillon <leo@thivillon.org>
2025-01-21 17:54:35 +0100

    s

--- installer.nix
+++ installer.nix
@@ -15,6 +15,8 @@ in
   virtualisation.vmware.guest.enable = true;
 
   isoImage.isoName = lib.mkForce "nixcademy-auto-installer-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}.iso";
+  isoImage.squashfsCompression = "zstd -T0 -Xcompression-level 3";
+
   services.getty.helpLine = ''
     ███╗   ██╗██╗██╗  ██╗ ██████╗ █████╗ ██████╗ ███████╗███╗   ███╗██╗   ██╗
     ████╗  ██║██║╚██╗██╔╝██╔════╝██╔══██╗██╔══██╗██╔════╝████╗ ████║╚██╗ ██╔╝
